3851bd676fd376ff7c04ff21580188cb7e696e1a
josuemj <98498772+josuemj@users.noreply.github.com>
2023-03-05 19:34:30 -0600

    Option 1 done.

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Controller {
 
@@ -18,6 +19,48 @@ public class Controller {
         return false;
     }
 
+    public static boolean isMapCategory(Map<String,ArrayList<String>> map,String cat){
+        for(String key: map.keySet()){
+            if(key.equals(cat)){
+                return true;
+           }
+        }
+        return false;
+    }
+
+    public static boolean isArticle(Map<String,ArrayList<String>> map, String article){
+        for(String key: map.keySet()){
+            for(int i = 0; i<map.get(key).size();i++){
+                try {
+                    if (map.get(key).get(i).equals(article)) {
+                        return true;
+                    }
+                }catch (Exception e){
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean productInCollections(Map <String,Integer> userMap,String product){
+        if (userMap.size() == 0){
+            return false;
+        }else{
+            for(String key:userMap.keySet()){
+                if (key.equals(product)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void iterateMap(Map<String,Integer> map){
+        for (String key: map.keySet()){
+            System.out.println("KEY: "+key+" VALUE: "+map.get(key));
+        }
+
+    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        //User collection
 
         while (true){
             System.out.println("Escoger implementacion\n1) HashMap\n2) TreeMap\n3) LinkeHashMap\n4) Salir");
@@ -20,7 +21,9 @@ public class Main {
             } else{
 
                 System.out.println("SHOWING MENU");
-                Map<String, ArrayList<String>> mapBuilt = MapBuilder.getInitialValues(map);
+                Map<String, ArrayList<String>> mapBuilt = MapBuilder.getInitialValues(map); //Inventory
+                Map<String,Integer> userCollection = MapFactory.getMap(mapType); //User Collection
+                //Were key:product value: the qt
 
                 while(true){
 
@@ -35,7 +38,39 @@ public class Main {
 
                     switch (menuSelection){
                         case "1":
-                            System.out.println(mapBuilt.size());
+                            while(true){ //Forced
+                                System.out.println("Ingresar la categoria del producto: ");
+                                String car = in.nextLine();
+
+                                if (Controller.isMapCategory(mapBuilt,car)){
+
+                                    System.out.println("Ingresar articulo");
+                                    String art = in.nextLine();
+
+                                    if(Controller.isArticle(mapBuilt,art)){
+
+                                        //All verified in this scope.
+                                        //Adding the article to user collection
+
+                                        if(Controller.productInCollections(userCollection,art)){
+                                            userCollection.put(art, userCollection.get(art)+1);
+                                            Controller.iterateMap(userCollection);
+                                        }else{
+                                            userCollection.put(art,1);
+                                            Controller.iterateMap(userCollection);
+                                        }
+                                        System.out.println("Artciulo agrado a la coleccion :)");
+
+                                        break;
+                                    }else{
+                                        System.out.println("El articulo no existe o no es de la categoria: "+car);
+                                    }
+
+                                }else{
+                                    System.out.println("La categoria no existe, ingresar de nuevo.");
+                                }
+                            }
+
                             break;
                         case "2":
                             break;
