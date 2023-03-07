5bc91900152b001bb0df340c1d40c9d80ab3a831
josuemj <98498772+josuemj@users.noreply.github.com>
2023-03-06 19:51:49 -0600

    last

---
+++ src/ImplementationTimes.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+
+public class ImplementationTimes {
+
+    public static void main(String[] args) {
+
+        Map myMap = MapBuilder.getInitialValues(MapFactory.getMap("3"));
+
+    }
+
+}
